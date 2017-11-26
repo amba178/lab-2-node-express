8ebdd5d1ab3b25da6d680a22bcd2f8b0a1a7af0c
Salem Amba <salemamba@gmail.com>
2017-11-26 15:16:51 -0800

    updates store object

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ let store = {
     url: 'https://webapplog.com/es6',
     text: 'This essay will give you a quick introduction to ES6. If you donâ€™t know what is ES6, itâ€™s a new JavaScript implementation.',
     comments: [
-      {text: 'Cruelâ€¦..var { house, mouse} = No type optimization at all'},
-      {text: 'I think youâ€™re undervaluing the benefit of â€˜letâ€™ and â€˜constâ€™.'},
-      {text: '(p1,p2)=>{ â€¦ } ,i understand this ,thank you !'} 
+      {text: "Cruel..var { house, mouse} = No type optimization at all"},
+      {text: "I think you're undervaluing the benefit of 'let' and 'const'."},
+      {text: "(p1,p2)=>{ ...} ,i understand this ,thank you !"} 
     ]
   }
 ]
